139303b01d457150cf88085d5619552aefffe9c7
Mauro Augusto Januário da Paixão <134173192+Gafanhoto-Mestre@users.noreply.github.com>
2025-11-14 12:18:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ function carregar(){
     if (hora >= 0 && hora < 12){
         img.src = 'manha.png'
         document.body.style.background = '#d1b384'
-        peri.innerHTML = "Bom dia 🙌! Você está no período da manhã."
+        peri.innerHTML = "Bom dia 🥱! Você está no período da manhã."
     } else if (hora >= 12 && hora <= 18){
         img.src = 'tarde.png'
         document.body.style.background = '#8b7b3b'
-        peri.innerHTML = "Bom dia 🙌! Você está no período da tarde."
+        peri.innerHTML = "Boa tarde 😎! Você está no período da tarde."
     } else {
         img.src = 'noite.png'
         document.body.style.background = '#1f2212c4'
-        peri.innerHTML = "Bom dia 🙌! Você está no período da tarde."
+        peri.innerHTML = "Boa noite 😴! Você está no período da noite."
     }
 }
