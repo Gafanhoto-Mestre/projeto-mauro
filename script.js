9083cd9c83e09edb8a1ae8f086a7c25c1ae6bd45
Mauro Augusto Januário da Paixão <134173192+Gafanhoto-Mestre@users.noreply.github.com>
2025-11-14 10:00:59 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    var peri = 
+window.document.getElementById('pr')
     var data =  new Date()
     var hora = data.getHours()
     var min = data.getMinutes()
@@ -8,6 +10,7 @@ function carregar(){
     if (hora >= 0 && hora < 12){
         img.src = 'manha.png'
         document.body.style.background = '#d1b384'
+        pr.innerHTML = "Bom dia 🙌! Você está no período da manhã"
     } else if (hora >= 12 && hora <= 18){
         img.src = 'tarde.png'
         document.body.style.background = '#8b7b3b'
