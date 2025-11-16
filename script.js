fbff068339532ac9b6979789abbdabca1ab22b80
Gafanhoto-Mestre <134173192+Gafanhoto-Mestre@users.noreply.github.com>
2025-11-15 22:57:20 -0300

    mapa

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function carregar(){
++function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var peri = window.document.getElementById('pr')
@@ -11,6 +11,7 @@ function carregar(){
         document.body.style.background = '#d1b384'
         peri.innerHTML = "Bom dia 🥱! Você está no período da manhã."
     } else if (hora >= 12 && hora <= 18){
+    } else if (hora >= 12 && hora < 18){
         img.src = 'tarde.png'
         document.body.style.background = '#8b7b3b'
         peri.innerHTML = "Boa tarde 😎! Você está no período da tarde."
