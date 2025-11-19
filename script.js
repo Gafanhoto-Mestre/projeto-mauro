0dbbbd4f35dfc6ae0058efc96b507581eb723439
Gafanhoto-Mestre <134173192+Gafanhoto-Mestre@users.noreply.github.com>
2025-11-19 10:44:11 -0300

    att

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ function carregar(){
     var data =  new Date()
     var hora = data.getHours()
     var min = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora}:${min} hs.`
+    msg.innerHTML = `${hora}:${min} hs.`
     if (hora >= 0 && hora < 12){
         img.src = 'manha.png'
-        document.body.style.background = '#d1b384'
+        document.body.style.background = 'linear-gradient(to top,#f4d96cff,#d1b384)'
         peri.innerHTML = "Bom dia 🥱!</br>Você está no período da manhã."
-    } else if (hora >= 12 && hora <= 18){
+    } else if (hora >= 12 && hora < 18){
         img.src = 'tarde.png'
-        document.body.style.background = '#8b7b3b'
+        document.body.style.background = 'linear-gradient(to top,#000000ff, #f4d96cff)'
         peri.innerHTML = "Boa tarde 😎!</br>Você está no período da tarde."
     } else {
         img.src = 'noite.png'
-        document.body.style.background = '#1f2212c4'
+        document.body.style.background = 'linear-gradient(to top, #C18B59,#000000ff)'
         peri.innerHTML = "Boa noite 😴!</br>Você está no período da noite."
     }
 }
